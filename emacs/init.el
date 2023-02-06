1a6540eee7584c3a9fa133716c3be751afd80153
Richard Nguyen <almachy@gmail.com>
2023-02-06 17:36:32 -0600

    Initial working version - Init.el - Configuration.org - README.org

---
+++ emacs/init.el
@@ -0,0 +1,15 @@
+;; Configure package.el to include MELPA
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(package-initialize)
+
+(defun package--save-selected-packages (&rest opt) nil)
+
+;; Ensure use-package is installed
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/dotfiles/emacs/configuration.org")
